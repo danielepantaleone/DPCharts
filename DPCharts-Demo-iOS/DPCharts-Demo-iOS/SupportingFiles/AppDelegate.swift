e4c10e0a2c104216a6b3f11ae242873f3d3f318d
Daniele Pantaleone <danielepantaleone@me.com>
2023-06-02 11:51:59 +0200

    Draft demo application structure

---
+++ DPCharts-Demo-iOS/DPCharts-Demo-iOS/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DPCharts-Demo-iOS
+//
+//  Created by Daniele Pantaleone
+//    - Github: https://github.com/danielepantaleone
+//    - LinkedIn: https://www.linkedin.com/in/danielepantaleone
+//
+//  Copyright © 2023 Daniele Pantaleone. Licensed under MIT License.
+//
+
+import Foundation
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
---
+++ DPCharts-Demo-iOS/DPCharts-Demo-iOS/ViewControllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DPCharts-Demo-iOS
+//
+//  Created by Daniele Pantaleone
+//    - Github: https://github.com/danielepantaleone
+//    - LinkedIn: https://www.linkedin.com/in/danielepantaleone
+//
+//  Copyright © 2023 Daniele Pantaleone. Licensed under MIT License.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
